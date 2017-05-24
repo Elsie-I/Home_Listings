a1f0eb13a5e9671de440b8b1b712f4cd45821311
pravesh <pravesh_shrestha87@yahoo.com>
2017-05-24 00:04:04 -0400

    messing with routes

---
+++ client/group_project/src/components/DetailsSinglePage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class DetailsSinglePage extends Component {
+  constructor(props){
+    super(props);
+  this.state={
+    homes:[],
+    }
+  }
+  componentDidMount() {
+    this.fetchSingleHome()
+  }
+
+
+  fetchSingleHome() {
+    fetch(`https://homelistings.herokuapp.com/api/homes/${this.props.ids}`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((responseJson) => {
+      console.log(responseJson)
+      //debugger
+      // this.setState((prevState) => {
+      //   return {
+      //     homes: responseJson.data.homes,
+      //   }
+      // });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>this is page for id {this.props.ids}</p>
+      </div>
+    )
+  }
+}
+export default DetailsSinglePage;
