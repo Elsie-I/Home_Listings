fee9704c889d4c4c277c5b06a38586bf5083da68
askflow1111 <askflow1111@gmail.com>
2017-05-24 10:58:11 -0400

    added parameters

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -4,9 +4,13 @@ const homesController = require('../controllers/homesController');
 const homeRoutes = express.Router();
 
 homeRoutes.get('/', homesController.index);
+homeRoutes.get('/results/zipcode=:zipcode&price=:price', homesController.showSpecific);
 homeRoutes.get('/:id', homesController.show);
 homeRoutes.post('/', homesController.create);
 homeRoutes.put('/:id', homesController.update);
 homeRoutes.delete('/:id', homesController.destroy);
+
+
+
 
 module.exports = homeRoutes;
